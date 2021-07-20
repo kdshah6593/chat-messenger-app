749ce175139ead07762d7d723cb9e100d4552176
Kunal <kdshah6593@gmail.com>
2021-07-20 11:37:42 -0400

    move socket io logic from www file to its own file

---
+++ server/socketApi.js
@@ -0,0 +1,34 @@
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+const onlineUsers = require("./onlineUsers");
+
+// Add your socket.io logic here!
+io.on("connection", (socket) => {
+    socket.on("go-online", (id) => {
+      if (!onlineUsers.includes(id)) {
+        onlineUsers.push(id);
+      }
+      // send the user who just went online to everyone else who is already online
+      socket.broadcast.emit("add-online-user", id);
+    });
+  
+    socket.on("new-message", (data) => {
+      socket.broadcast.emit("new-message", {
+        message: data.message,
+        sender: data.sender,
+      });
+    });
+  
+    socket.on("logout", (id) => {
+      if (onlineUsers.includes(id)) {
+        userIndex = onlineUsers.indexOf(id);
+        onlineUsers.splice(userIndex, 1);
+        socket.broadcast.emit("remove-offline-user", id);
+      }
+    });
+  });
+// end of socket.io logic
+
+module.exports = socketapi;
